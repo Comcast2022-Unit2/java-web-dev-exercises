aad570b49867d734a725198e1324f3c9cc9eb9f7
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-08-11 12:03:00 -0500

    Completed Parts 1&2

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,9 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        // let rover: Rover = new Rover(1);
+        Student pearce = new Student("Pearce", 123, 1, 4.0);
+//        pearce.setName("Pearce");
+//        pearce.setStudentId(1);
     }
 }
